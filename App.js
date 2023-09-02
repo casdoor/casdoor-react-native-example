2e4091dbdfc2971a0010fb74d19ec953eba84684
ChenWenpeng <wenpengchen@njust.edu.cn>
2023-09-02 21:57:14 +0800

    feat: add Casdoor SSO login (#3)
    * feat: add Casdoor Web SSO login.
    
    * fix: fix Readme.
    
    * fix: remove comments.
    
    * feat: use casdoor-react-native-sdk.
    
    * fix: Update package.json
    
    * fix: update.yarn.lock
    
    * Update yarn.lock
    
    * Update yarn.lock
    
    * Update yarn.lock

--- App.js
+++ App.js
@@ -6,107 +6,175 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, { useState } from 'react';
+import { WebView } from 'react-native-webview';
+import {Image, LogBox, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, useColorScheme, View} from 'react-native';
+import SDK from 'casdoor-react-native-sdk';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
+const App = () => {
+  LogBox.ignoreAllLogs();
   const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
+    backgroundColor: isDarkMode ? 'black' : 'white',
   };
 
+  const sdkConfig = {
+    serverUrl: 'https://door.casdoor.com',
+    clientId: 'b800a86702dd4d29ec4d',
+    clientSecret: '1219843a8db4695155699be3a67f10796f2ec1d5',
+    appName: 'app-example',
+    organizationName: 'casbin',
+    redirectPath: 'http://localhost:5000/callback',
+    signinPath: '/api/signin',
+  };
+  const sdk = new SDK(sdkConfig);
+
+  const [webViewVisible, setWebViewVisible] = useState(false);
+  const [scrollViewVisible, setScrollViewVisible] = useState(true);
+  const [userInfo, setUserInfo] = useState(null);
+  const [casdoorLoginURL, setCasdoorLoginURL] = useState('');
+
+  const handleCasdoorLogin = async () => {
+    setScrollViewVisible(false);
+    setWebViewVisible(true);
+
+    const signinUrl = await sdk.getSigninUrl();
+    setCasdoorLoginURL(signinUrl);
+  };
+
+  const handleLogout = () => {
+    setUserInfo(null);
+    setWebViewVisible(false);
+    setScrollViewVisible(true);
+    sdk.clearState();
+  };
+
+  const onNavigationStateChange = async(navState) => {
+    if (navState.url.startsWith(sdkConfig.redirectPath)) {
+      const token = await sdk.getAccessToken(navState.url);
+      const userInfo = sdk.JwtDecode(token);
+      setUserInfo(userInfo);
+
+      setWebViewVisible(false);
+      setScrollViewVisible(false);
+    }
+  };
+
+  const renderWebview = webViewVisible && (
+      <WebView
+          source={{ uri: casdoorLoginURL }}
+          onNavigationStateChange={onNavigationStateChange}
+          style={{ flex: 1 }}
+          mixedContentMode='always'
+      />
+  );
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        {renderWebview}
+        {scrollViewVisible && (
+            <View style={{ flex: 1 }}>
+              <ScrollView contentInsetAdjustmentBehavior='automatic' style={backgroundStyle}>
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity style={styles.button} onPress={handleCasdoorLogin}>
+                    <Text style={styles.buttonText}>Login with Casdoor</Text>
+                  </TouchableOpacity>
+                </View>
+              </ScrollView>
+            </View>
+        )}
+        {userInfo && (
+            <View style={{ flex: 1 }}>
+              <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+                {userInfo && (
+                    <View style={styles.userInfoContainer}>
+                      <View style={styles.userInfoCard}>
+                        <Text style={styles.userInfoTitle}>User Information</Text>
+                        <View style={styles.avatarContainer}>
+                          <Image
+                              source={{ uri: userInfo.avatar }}
+                              style={styles.avatar}
+                          />
+                        </View>
+                        <Text style={styles.userInfoText}>{userInfo.name}</Text>
+                        <Text style={styles.userInfoText}>{userInfo.email}</Text>
+                        {/* add other userInfo here. */}
+                      </View>
+                      <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+                        <Text style={styles.buttonText}>Logout</Text>
+                      </TouchableOpacity>
+                    </View>
+                )}
+              </ScrollView>
+            </View>
+        )}
+      </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 400,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  button: {
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 5,
   },
-  sectionDescription: {
-    marginTop: 8,
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  userInfoContainer: {
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  userInfoCard: {
+    backgroundColor: '#f0f0f0',
+    padding: 20,
+    borderRadius: 10,
+    marginTop: 200,
+    marginBottom: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  userInfoTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  userInfoText: {
+    textAlign: 'center',
     fontSize: 18,
-    fontWeight: '400',
+    marginBottom: 8,
   },
-  highlight: {
-    fontWeight: '700',
+  logoutButton: {
+    backgroundColor: '#FF6347',
+    padding: 12,
+    borderRadius: 5,
+  },
+  avatarContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
 });
-
 export default App;
--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # casdoor-react-native-example
+
+This example uses [casdoor-react-native-sdk](https://github.com/casdoor/casdoor-react-native-sdk).
+This example describes how to use casdoor in [react-native](https://reactnative.dev/).
+
+## Quick Start
+
+- download the code
+
+```bash
+ git clone git@github.com:casdoor/casdoor-react-native-example.git
+```
+
+- install dependencies
+```bash 
+ cd casdoor-react-native-example
+ yarn install
+ cd ios/
+ pod install
+```
+- run on ios
+```bash
+cd casdoor-react-native-example
+react-native start
+react-native run-ios
+```
+- run on android
+```bash
+cd casdoor-react-native-example
+react-native start
+react-native run-android
+```
+>Make sure to turn on the emulator or real device before running.
+
+## After running, you will see the following  interfaces:
+
+|                           **iOS**                           |                         **Android**                          |
+| :---------------------------------------------------------: | :----------------------------------------------------------: |
+| <img src="./iOS-gif.gif" alt="iOS-gif" style="zoom:30%;" /> | <img src="./Android-gif.gif" alt="Android-gif" style="zoom: 30%;" /> |
+
+
+## Configure
+
+Initialization requires 5 parameters, which are all str type:
+| Name         | Must | Description                                            |
+| ------------ | ---- | ------------------------------------------------------ |
+| serverUrl    | Yes  | Casdoor Server Url, such as `https://door.casdoor.com` |
+| redirectPath | Yes  | redirectPath                                           |
+| appName      | Yes  | Application name                                       |
+| clientId     | Yes  | Your client id                                         |
+| clientSecret | Yes  | Your client secret                                     |
+
+```javascript
+  const sdkConfig = {
+  serverUrl: 'https://door.casdoor.com',
+  clientId: 'b800a86702dd4d29ec4d',
+  clientSecret: '1219843a8db4695155699be3a67f10796f2ec1d5',
+  appName: 'app-example',
+  organizationName: 'casbin',
+  redirectPath: 'http://localhost:5000/callback',
+  signinPath: '/api/signin',
+};
+```
+
+## License
+
+This project is licensed under the [Apache 2.0 license](https://github.com/casdoor/casdoor-dotnet-sdk/blob/master/LICENSE).
